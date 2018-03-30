90c2733dfef2e49f97e65511f28e71fa3fd20548
Robert Czechowski <robert+git@czecho.de>
2018-03-30 13:26:41 +0200

    Add default config and metadata

---
+++ conf/default.php
@@ -0,0 +1,5 @@
+$conf['protocol'] = 'pgsql';
+$conf['server']   = 'localhost';
+$conf['user']     = 'dbuser';
+$conf['db']       = 'dbname';
+$conf['password'] = 'dbpassword';
---
+++ conf/metadata.php
@@ -0,0 +1,5 @@
+$meta['protocol'] = array('multichoice','_choices' => array('pgsql','mysql','sqlite');
+$meta['server']   = array('string');
+$meta['user']     = array('string');
+$meta['db']       = array('string');
+$meta['password'] = array('string');
