0fb1aa7e0fcd2f56793595277cfb82c8d61b180a
Robert Czechowski <robert@code-intelligence.com>
2025-04-18 00:01:48 +0200

    Fix Plugin: Add missing semicolons to lang/en/settings.php

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -4,6 +4,6 @@ $lang['server']      = 'Hostname or file (sqlite) of Django database';
 $lang['user']        = 'Database username of Django database (leave empty for sqlite)';
 $lang['db']          = 'Database name of Django database (leave empty for sqlite)';
 $lang['password']    = 'Database password for Django database (leave empty for sqlite)';
-$lang['logoff_uri']  = 'URI that can be used for users to log out, e.g. "https://example.com/accounts/logout/?next=/wiki/". If left empty, no logout button will be shown in the dokuwiki.'
-$lang['admin_admin'] = 'Give Django admin accounts admin rights in the dokuwiki'
-$lang['staff_admin'] = 'Give Django staff accounts admin rights in the dokuwiki'
+$lang['logoff_uri']  = 'URI that can be used for users to log out, e.g. "https://example.com/accounts/logout/?next=/wiki/". If left empty, no logout button will be shown in the dokuwiki.';
+$lang['admin_admin'] = 'Give Django admin accounts admin rights in the dokuwiki';
+$lang['staff_admin'] = 'Give Django staff accounts admin rights in the dokuwiki';
